6dc6397730f41e462dc477f82599d76ce4b11836
cianando <cianandopautrisio@gmail.com>
2023-11-13 22:08:12 +0700

    Hias-hias

--- flaskclassifier-master/main.py
+++ flaskclassifier-master/main.py
@@ -1,5 +1,5 @@
 import os
-from app import app, bootstrap
+from app import app
 from predict_classes import  local_classes
 from flask import Flask, flash, request, redirect, url_for, render_template
 from werkzeug.utils import secure_filename
